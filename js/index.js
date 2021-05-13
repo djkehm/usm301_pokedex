1fcc0971191fe868f66b71388d901856a32170d0
Kehmit Uribe <kehmit.uribeg@gmail.com>
2021-05-13 13:24:18 -0400

    Modificar domo, agregar tinymce

--- js/index.js
+++ js/index.js
@@ -1,4 +1,30 @@
+
+tinymce.init({
+    selector: '#descripcion-txt',
+    height: 200,
+    menubar: false,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar: 'undo redo | formatselect | ' +
+    'bold italic backcolor | alignleft aligncenter ' +
+    'alignright alignjustify | bullist numlist outdent indent | ' +
+    'removeformat | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  });
+  
+  
+
+
 document.querySelector("#btn-registrar").addEventListener("click",()=>{
+    //value es para obtener el valor de los input de texto
     let nombre = document.querySelector("#nombre-txt").value;
-    alert("Hola Mundo!", nombre);
+    //Esto lo saque de la pagina del tinymce, es para obtener lo escrito
+    let descripcion = tinymce.get("descripcion-txt").getContent();
+    //checked 
+    let legendario = document.querySelector("#legendario-si").checked;
+    let tipo = document.querySelector("#tipo-select").value
+    console.log("Hola Mundo!", nombre, descripcion, legendario, tipo);
 });
